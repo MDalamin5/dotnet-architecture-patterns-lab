de809cddadbc17db22f3644c8b291c95802266f2
Md Al Amin <mdal.amin5@northsouth.edu>
2026-09-20 10:30:11 +0600

    Desigination we setup the Generic repository setup

--- OrganizationManagement/Repository/IRepository/IDesignationRepository.cs
+++ OrganizationManagement/Repository/IRepository/IDesignationRepository.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using OrganizationManagement.DTOs.Designation;
+using OrganizationManagement.Models;
 
 namespace OrganizationManagement.Repository.IRepository
 {
-    public interface IDesignationRepository
+    public interface IDesignationRepository: IGenericRepository<Designation>
     {
-        Task<bool> CreateDesignation(CreateDesignationDto model);
-        Task<List<ReadDesignationDto>> getAllDesignation();
-        Task<ReadDesignationDto> getDesignationById(Guid id);
-        Task<bool> updateDesignationById(Guid id, UpdateDesignationDto model);
-        Task<bool> deleteDesignationById(Guid id);
+        // Task<bool> CreateDesignation(CreateDesignationDto model);
+        // Task<List<ReadDesignationDto>> getAllDesignation();
+        // Task<ReadDesignationDto> getDesignationById(Guid id);
+        // Task<bool> updateDesignationById(Guid id, UpdateDesignationDto model);
+        // Task<bool> deleteDesignationById(Guid id);
     }
 }
