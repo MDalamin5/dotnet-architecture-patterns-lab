2ec2638f865762ced8ca07b97685214ddfd22af6
Md Al Amin <mdal.amin5@northsouth.edu>
2026-09-20 12:14:09 +0600

    Unit of work Interfaces implementations

---
+++ OrganizationManagement/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OrganizationManagement.Models;
+
+namespace OrganizationManagement.Repository.IRepository
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        IGenericRepository<Department> Departments {get;}
+        IGenericRepository<Designation> Designations {get;}
+
+        Task<int> SaveChangesAsync();
+        
+    }
+}
