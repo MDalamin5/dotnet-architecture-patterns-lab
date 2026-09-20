1c2cb9771eed6feda3ca0aa49c10f63d9a320645
Md Al Amin <mdal.amin5@northsouth.edu>
2026-09-20 10:36:36 +0600

    remove few unnecessary code from the repository file

--- OrganizationManagement/Repository/DepartmentRepository.cs
+++ OrganizationManagement/Repository/DepartmentRepository.cs
@@ -19,57 +19,6 @@ namespace OrganizationManagement.Repository
             
         }
 
-        // public async Task<bool> createDepartments(DepartmentCreateDto model)
-        // {
-        //     var data = new Department
-        //     {
-        //         Id = Guid.NewGuid(),
-        //         Name = model.Name
-        //     };
-
-        //     Console.WriteLine($"Name: {data.Name}, Id: {data.Id}");
-            
-        //     await _appDbContext.Departments.AddAsync(data);
-        //     await _appDbContext.SaveChangesAsync();
-
-        //     return true;
-        // }
-
-        // public async Task<List<DepartmentReadDto>> getAllDepartments()
-        // {
-        //      var allDepartments = await _appDbContext.Departments.Select(d => new DepartmentReadDto
-        //      {
-        //          Name = d.Name
-        //      }).ToListAsync();
-        //     return allDepartments;
-        // }
-
-        // public async Task<DepartmentReadDto> getDepartmentById(Guid id)
-        // {
-        //     var dbObj = await _appDbContext.Departments.FirstOrDefaultAsync(d => d.Id == id);
-
-        //     return new DepartmentReadDto
-        //     {
-        //         Name = dbObj.Name
-        //     };
-        // }
-
-        // public async Task<bool> updateDepartmentById(Guid id, DepartmentUpdateDto model)
-        // {
-        //     var dbObj = await _appDbContext.Departments.FirstOrDefaultAsync(d => d.Id == id);
-        //     dbObj.Name = model.Name;
-        //     await _appDbContext.SaveChangesAsync();
-
-        //     return true;
-        // }
-
-        // public async Task<bool> deleteDepartmentById(Guid id)
-        // {
-        //     var dbObj = await _appDbContext.Departments.FirstOrDefaultAsync(d => d.Id == id);
-        //     _appDbContext.Departments.Remove(dbObj);
-        //     await _appDbContext.SaveChangesAsync();
-
-        //     return true;
-        // }
+        
     }
 }
--- OrganizationManagement/Repository/DesignationRepository.cs
+++ OrganizationManagement/Repository/DesignationRepository.cs
@@ -21,58 +21,6 @@ namespace OrganizationManagement.Repository
                 
             }
 
-            // public async Task<bool> CreateDesignation(CreateDesignationDto model)
-            // {
-            //     var data = new Designation
-            //     {
-            //         Id = Guid.NewGuid(),
-            //         Name = model.Name
-            //     };
-
-            //     Console.WriteLine($"Name: {data.Name}, Id: {data.Id}");
-                
-            //     await _appDbContext.Designations.AddAsync(data);
-            //     await _appDbContext.SaveChangesAsync();
-
-            //     return true;
-            // }
-
-            // public async Task<List<ReadDesignationDto>> getAllDesignation()
-            // {
-            //     var allDeg = await _appDbContext.Designations.Select(d => new ReadDesignationDto
-            //     {
-            //         Name = d.Name
-            //     }).ToListAsync();
-
-            //     return allDeg;
-            // }
-
-            // public async Task<ReadDesignationDto> getDesignationById(Guid id)
-            // {
-            //     var dbObj = await _appDbContext.Designations.FirstOrDefaultAsync(d => d.Id == id);
-
-            //     return new ReadDesignationDto
-            //     {
-            //         Name = dbObj.Name
-            //     };
-            // }
-
-            // public async Task<bool> updateDesignationById(Guid id, UpdateDesignationDto model)
-            // {
-            //     var dbObj = await _appDbContext.Designations.FirstOrDefaultAsync(d => d.Id == id);
-            //     dbObj.Name = model.Name;
-            //     await _appDbContext.SaveChangesAsync();
-
-            //     return true;
-            // }
-
-            // public async Task<bool> deleteDesignationById(Guid id)
-            // {
-            //     var dbObj = await _appDbContext.Designations.FirstOrDefaultAsync(d => d.Id == id);
-            //     _appDbContext.Designations.Remove(dbObj);
-            //     await _appDbContext.SaveChangesAsync();
-
-            //     return true;
-            // }
+            
         }
 }
